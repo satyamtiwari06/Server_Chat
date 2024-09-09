720f51053cab98937924f10dce410c953465b37e
Satyam Tiwari <st9807419005@gmail.com>
2024-09-10 00:41:33 +0530

    Finished Backend

---
+++ constants/config.js
@@ -0,0 +1,11 @@
+const corsOptions = {
+  origin: [
+    process.env.CLIENT_URL,
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+};
+
+const CHATTU_TOKEN = "chattu-token";
+
+export { corsOptions, CHATTU_TOKEN };
